f1190c19e3d3ab4971240258e738ece4595044d1
Lucía Martínez <lucimart@student.42madrid.com>
2020-09-08 22:02:37 +0200

    Corrected some style issues

--- src/components/Blog/blog.component.jsx
+++ src/components/Blog/blog.component.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './blog.styles.scss';
 
 const Blog = () => {
-  return <section></section>;
+  return (
+    <section className='blog'>
+      On Construction, for you{' '}
+      <span role='img' aria-label='😉 Wink Emoji'>
+        😉
+      </span>
+    </section>
+  );
 };
 
 export default Blog;
