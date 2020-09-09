72d34fc5b9a0a436c3b57fcdab29b570ce9c3f80
Lucía Martínez <lucimart@student.42madrid.com>
2020-09-09 18:51:52 +0200

    add waves, fight a lot

--- src/components/Blog/blog.component.jsx
+++ src/components/Blog/blog.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './blog.styles.scss';
-
 const Blog = () => {
   return (
-    <section className='blog'>
-      On Construction, for you{' '}
-      <span role='img' aria-label='😉 Wink Emoji'>
-        😉
-      </span>
-    </section>
+    <main className='blog'>
+      <p>
+        On Construction, for you{' '}
+        <span role='img' aria-label='😉 Wink Emoji'>
+          😉
+        </span>
+      </p>
+    </main>
   );
 };
 
--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -3,17 +3,15 @@ import './footer.styles.scss';
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      {/* <div className='footer__wave'>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          id='footer-wave'
-          viewBox='0 0 800 338.05'
-          preserveAspectRatio='none'
-        >
-          <path> </path>
-        </svg>
-      </div> */}
+    <footer className='footr'>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        id='footer-wave'
+        viewBox='0 0 800 308.05'
+        preserveAspectRatio='none'
+      >
+        <path></path>
+      </svg>
     </footer>
   );
 };
